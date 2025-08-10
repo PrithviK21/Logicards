49cf34042810df3ea6a1fb495d6e0777cd401335
Prithvi Kumar <prithvikumar@hotmail.com>
2025-08-10 13:56:28 +0530

    trying relative image path

--- src/components/Game/Card/Card.tsx
+++ src/components/Game/Card/Card.tsx
@@ -1,14 +1,22 @@
-import type { CardProps } from '../../../types';
+import type { CardProps } from "../../../types";
 
-function Card({ details, onClick, isSelected, isActive, canSelect }: CardProps) {
-  const imgPath = `/images/cards/${!details.isFlipped ? 'backs/' : ''}${details.value}.png`;
+function Card({
+  details,
+  onClick,
+  isSelected,
+  isActive,
+  canSelect,
+}: CardProps) {
+  const imgPath = `images/cards/${!details.isFlipped ? "backs/" : ""}${
+    details.value
+  }.png`;
 
   return (
     <div
-      className={`card ${isSelected ? 'selected' : ''} 
-      ${isActive ? 'active' : ''}
-      ${canSelect ? 'can-select' : ''}
-      ${details.isFlipped ? 'flipped' : ''}`}
+      className={`card ${isSelected ? "selected" : ""} 
+      ${isActive ? "active" : ""}
+      ${canSelect ? "can-select" : ""}
+      ${details.isFlipped ? "flipped" : ""}`}
       onClick={() => onClick(details.value)}
     >
       <img src={imgPath} alt={`Card with ${details.value} on it`} />
