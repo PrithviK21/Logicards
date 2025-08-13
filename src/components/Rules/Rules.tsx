b089547b5c960f6b4c6385f2ac1edccf977e39e0
Prithvi Kumar <prithvikumar@hotmail.com>
2025-08-13 17:48:15 +0530

    Removed page 2 of the rules

--- src/components/Rules/Rules.tsx
+++ src/components/Rules/Rules.tsx
@@ -1,5 +1,4 @@
 import pg1 from "../../assets/images/rules/pg1.png";
-import pg2 from "../../assets/images/rules/pg2.png";
 function Rules({ returnToMenu }: { returnToMenu: () => void }) {
   return (
     <>
@@ -8,7 +7,6 @@ function Rules({ returnToMenu }: { returnToMenu: () => void }) {
       </button>
       <div className="rules">
         <img src={pg1} alt="Rules page 1" />
-        <img src={pg2} alt="Rules page 2" />
       </div>
     </>
   );
